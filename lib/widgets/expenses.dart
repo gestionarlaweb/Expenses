5beca7a0744cff2263e80bbad2bdde8d002d8feb
David <gestionarlaweb@gmail.com>
2024-09-19 16:01:54 +0200

    ADD Grafica

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,7 +1,9 @@
-import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
+
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/chart/chart.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -51,7 +53,7 @@ class _ExpensesState extends State<Expenses> {
     setState(() {
       _registeredExpenses.remove(expense);
     });
-    // clearSnackBars() eliminar todos los snackbars que estén actualmente 
+    // clearSnackBars() eliminar todos los snackbars que estén actualmente
     // mostrados en la pantalla
     ScaffoldMessenger.of(context).clearSnackBars();
     ScaffoldMessenger.of(context).showSnackBar(
@@ -94,7 +96,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text('The chart'),
+          Chart(expenses: _registeredExpenses),
           Expanded(
             child: mainContent,
           ),
--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -19,6 +19,11 @@ class ExpensesList extends StatelessWidget {
       // Dismissible -> Efecto de deslizamiento
       itemBuilder: (ctx, index) => Dismissible(
         key: ValueKey(expenses[index]),
+        background: Container(
+          color: Theme.of(context).colorScheme.error.withOpacity(0.75),
+          margin: EdgeInsets.symmetric(
+            horizontal: Theme.of(context).cardTheme.margin!.horizontal),
+          ),
         onDismissed: (direction) {
           onRemoveExpense(expenses[index]);
         },
